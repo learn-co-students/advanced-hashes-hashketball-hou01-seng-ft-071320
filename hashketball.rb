2b27b321067e960cb5c8e83e668dcec9ebd5b937
John Doan <johndoan42@gmail.com>
2020-06-24 21:55:14 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
 # Write your code below game_hash
+require 'pry'
 def game_hash
   {
     home: {
@@ -127,3 +128,82 @@ def game_hash
 end
 
 # Write code here
+def num_points_scored(basketball_player)
+  points = "Player not on Team"
+  game_hash.each do |key,value|
+    value[:players].each do |index|
+      if index[:player_name] == basketball_player
+        points = index[:points]
+      end
+    end
+  end
+  points
+end
+
+def shoe_size(basketball_player)
+  shoe_size = "Player not on Team"
+  game_hash.each do |key,value|
+    value[:players].each do |index|
+      if index[:player_name] == basketball_player
+        shoe_size = index[:shoe]
+      end
+    end
+  end
+  shoe_size
+end
+
+def team_colors(team_name)
+  team_colors = "Team does not Exist"
+  game_hash.each do |key,value|
+    if value[:team_name] == team_name
+      team_colors = value[:colors]
+    end
+  end
+  team_colors
+end
+
+def team_names
+  names_of_teams = game_hash.each_with_object([]) do |(key,value),team_array|
+    team_array << value[:team_name]
+  end
+  names_of_teams
+end
+
+def player_numbers(team_name)
+  number_in_team = []
+  game_hash.each do |key,value|
+    if value[:team_name] == team_name
+      value[:players].each do |index|
+        number_in_team << index[:number]
+      end
+    end
+  end
+  number_in_team
+end
+
+def player_stats(basketball_player)
+  stats = "Player not on Team"
+  game_hash.each do |key,value|
+    value[:players].each do |index|
+      if index[:player_name] == basketball_player
+        stats = index
+      end
+    end
+  end
+  stats
+end
+
+def big_shoe_rebounds
+  #returns the number of rebounds of the player with the biggest shoe size
+  shoe_size = 0
+  rebound = 0
+  game_hash.each do |key,value|
+    value[:players].each do |index|
+      if index[:shoe] > shoe_size
+        shoe_size = index[:shoe]
+        rebound = index[:rebounds]
+      end
+    end
+  end
+  rebound
+end
